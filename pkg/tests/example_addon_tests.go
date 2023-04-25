34b3f81b6a4a1cf75226d916db4da9bf3d3039f9
Brady Pratt <bpratt@redhat.com>
2023-04-25 12:20:00 -0500

    cleanup example test harness tests
    Signed-off-by: Brady Pratt <bpratt@redhat.com>

--- pkg/tests/example_addon_tests.go
+++ pkg/tests/example_addon_tests.go
@@ -6,51 +6,47 @@ import (
 	"github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	"github.com/openshift/osde2e-example-test-harness/pkg/metadata"
-
-	"k8s.io/apiextensions-apiserver/pkg/client/clientset/clientset"
+	apiextclientset "k8s.io/apiextensions-apiserver/pkg/client/clientset/clientset"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
-
 	"k8s.io/client-go/rest"
 )
 
-// Using Reference Addon for test
-var crdName = "referenceaddons.reference.addons.managed.openshift.io"
-
 var _ = ginkgo.Describe("Example Addon Tests", func() {
-	defer ginkgo.GinkgoRecover()
-	config, err := rest.InClusterConfig()
+	var config *rest.Config
 
-	if err != nil {
-		panic(err)
-	}
+	ginkgo.BeforeEach(func() {
+		var err error
+		config, err = rest.InClusterConfig()
+		Expect(err).NotTo(HaveOccurred(), "unable to load in cluster config")
+	})
 
-	ginkgo.It("Example CRD - "+crdName+" - exists", func() {
-		apiextensions, err := clientset.NewForConfig(config)
-		Expect(err).NotTo(HaveOccurred())
+	ginkgo.It("CRD addons.addons.managed.openshift.io exists", func() {
+		client, err := apiextclientset.NewForConfig(config)
+		Expect(err).NotTo(HaveOccurred(), "failed to create clientset")
 
 		// Make sure the CRD exists
-		result, err := apiextensions.ApiextensionsV1().CustomResourceDefinitions().Get("addons.addons.managed.openshift.io", v1.GetOptions{})
-
+		result, err := client.ApiextensionsV1().CustomResourceDefinitions().Get("addons.addons.managed.openshift.io", v1.GetOptions{})
 		if err != nil {
 			log.Printf("CRD not found: %v", err.Error())
 			metadata.Instance.FoundCRD = false
 		} else {
-			log.Printf("CRD found: %v", result)
+			log.Printf("CRD found: %v", result.GetName())
 			metadata.Instance.FoundCRD = true
 		}
 
-		Expect(err).NotTo(HaveOccurred())
+		Expect(err).NotTo(HaveOccurred(), "failed to get the crd")
 	})
 
 	ginkgo.It("Example passthrough secret exists", func() {
 		k8s, err := kubernetes.NewForConfig(config)
-		Expect(err).NotTo(HaveOccurred())
-		sec, err := k8s.CoreV1().Secrets("osde2e-ci-secrets").Get("ci-secrets", v1.GetOptions{})
+		Expect(err).NotTo(HaveOccurred(), "unable to create client")
 
-		Expect(sec.Data["testkey"]).ToNot(BeNil())
-		Expect(err).NotTo(HaveOccurred())
+		namespace := "osde2e-ci-secrets"
+		name := "ci-secrets"
 
+		sec, err := k8s.CoreV1().Secrets(namespace).Get(name, v1.GetOptions{})
+		Expect(err).NotTo(HaveOccurred(), "failed to fetch %s/%s secret", namespace, name)
+		Expect(sec.Data).Should(HaveKey("testkey"))
 	})
-
 })
