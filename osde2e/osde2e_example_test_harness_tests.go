baca27bd5841f14ef4ddeb14aaa693d7ae0ef3e1
Ritu Mundhe <rmundhe@redhat.com>
2023-12-05 09:54:30 -0600

    harness update

--- osde2e/osde2e_example_test_harness_tests.go
+++ osde2e/osde2e_example_test_harness_tests.go
@@ -5,12 +5,16 @@
 package osde2etests
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/onsi/ginkgo/v2"
 	"github.com/onsi/gomega"
 )
 
 var _ = ginkgo.Describe("osde2e-example-test-harness", func() {
 	ginkgo.It("Makes simple assertion", func() {
+		fmt.Printf("timeout provided %s", os.Getenv("HARNESS_TIMEOUT"))
 		gomega.Expect(1).Should(gomega.Equal(1), "one should equal one")
 	})
 
