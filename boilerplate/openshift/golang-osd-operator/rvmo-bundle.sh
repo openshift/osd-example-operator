d67168df9faf26d2410c64865880b423369f4771
Mariusz Mazur <mmazur@redhat.com>
2024-06-24 16:08:03 +0200

    Autodetect current branch for rvmo

--- boilerplate/openshift/golang-osd-operator/rvmo-bundle.sh
+++ boilerplate/openshift/golang-osd-operator/rvmo-bundle.sh
@@ -3,7 +3,8 @@
 set -e
 
 REPOSITORY=${REPOSITORY:-"https://github.com/openshift/managed-release-bundle-osd.git"}
-BRANCH=${BRANCH:-main}
+CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+BRANCH=${BRANCH:-$CURRENT_BRANCH}
 DELETE_TEMP_DIR=${DELETE_TEMP_DIR:-true}
 TMPD=$(mktemp -d --suffix -rvmo-bundle)
 [[ "${DELETE_TEMP_DIR}" == "true" ]] && trap 'rm -rf ${TMPD}' EXIT
