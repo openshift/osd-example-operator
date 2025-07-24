b114a29672a2f3e5d9a68e76a3520f25d3a9be3f
bpratt <bpratt@redhat.com>
2025-07-24 09:43:56 -0500

    SDCICD-1612: rescaffold controller
    so that it is fully installable and reconciling an Example custom
    resource definition
    
    Follow the SOP on setting up an operator, with some git shenanigans to
    keep existing files
    
    Signed-off-by: bpratt <bpratt@redhat.com>

--- Makefile
+++ Makefile
@@ -1,12 +1,6 @@
-FIPS_ENABLED=true
-HARNESS_TIMEOUT=690
-
-export KONFLUX_BUILDS=true
-
 include boilerplate/generated-includes.mk
-SHELL := /usr/bin/env bash
-# needed for internal saas file as boilerplate checks commercial app-interface saas file hashes
-export SKIP_SAAS_FILE_CHECKS=y
+
+
 .PHONY: boilerplate-update
 boilerplate-update:
 	@boilerplate/update
--- internal/controller/example_controller.go
+++ internal/controller/example_controller.go
@@ -1,5 +1,5 @@
 /*
-Copyright 2023.
+Copyright 2025.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-package controllers
+package controller
 
 import (
 	"context"
@@ -22,9 +22,9 @@ import (
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
-	"sigs.k8s.io/controller-runtime/pkg/log"
+	logf "sigs.k8s.io/controller-runtime/pkg/log"
 
-	cachev1alpha1 "github.com/openshift/osd-example-operator/api/v1alpha1"
+	managedv1alpha1 "github.com/openshift/osd-example-operator/api/v1alpha1"
 )
 
 // ExampleReconciler reconciles a Example object
@@ -33,21 +33,12 @@ type ExampleReconciler struct {
 	Scheme *runtime.Scheme
 }
 
-//+kubebuilder:rbac:groups=managed.openshift.io,resources=Examples,verbs=get;list;watch;create;update;patch;delete
-//+kubebuilder:rbac:groups=managed.openshift.io,resources=Examples/status,verbs=get;update;patch
-//+kubebuilder:rbac:groups=managed.openshift.io,resources=Examples/finalizers,verbs=update
+// +kubebuilder:rbac:groups=managed.openshift.io,resources=examples,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=managed.openshift.io,resources=examples/status,verbs=get;update;patch
+// +kubebuilder:rbac:groups=managed.openshift.io,resources=examples/finalizers,verbs=update
 
-// Reconcile is part of the main kubernetes reconciliation loop which aims to
-// move the current state of the cluster closer to the desired state.
-// TODO(user): Modify the Reconcile function to compare the state specified by
-// the Example object against the actual cluster state, and then
-// perform operations to make the cluster state reflect the state specified by
-// the user.
-//
-// For more details, check Reconcile and its Result here:
-// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.14.1/pkg/reconcile
 func (r *ExampleReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	_ = log.FromContext(ctx)
+	_ = logf.FromContext(ctx)
 
 	// TODO(user): your logic here
 
@@ -57,6 +48,7 @@ func (r *ExampleReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ct
 // SetupWithManager sets up the controller with the Manager.
 func (r *ExampleReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
-		For(&cachev1alpha1.Example{}).
+		For(&managedv1alpha1.Example{}).
+		Named("example").
 		Complete(r)
 }
