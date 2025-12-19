c19625776e08c79c61378d418547fb0ddee76893
ritmun <rmundhe@redhat.com>
2025-12-19 12:04:40 -0600

    add test for ocm client

--- test/e2e/osd_example_operator_tests.go
+++ test/e2e/osd_example_operator_tests.go
@@ -8,11 +8,24 @@ import (
 	"fmt"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+	ocme2e "github.com/openshift/osde2e-common/pkg/clients/ocm"
+	"os"
 )
 
 var _ = Describe("osd-example-operator", func() {
 	It("asserts success", func(ctx context.Context) {
 		Expect(true).To(BeTrue(), "True should be true")
+	})
+
+	It("should connect to stage ocm client", func(ctx context.Context) {
+		By("Getting ocm creds")
+		clientID := os.Getenv("OCM_CLIENT_ID")
+		clientSecret := os.Getenv("OCM_CLIENT_SECRET")
+		Expect(clientID).NotTo(BeEmpty(), "OCM_CLIENT_ID must be set")
+		Expect(clientSecret).NotTo(BeEmpty(), "OCM_CLIENT_SECRET must be set")
+		ocmEnv := ocme2e.Stage
+		_, err := ocme2e.New(ctx, "", clientID, clientSecret, ocmEnv)
+		Expect(err).ShouldNot(HaveOccurred(), "Unable to setup stage OCM Client")
 	})
 
 	// Failing test for log analysis demo
